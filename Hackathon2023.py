2e2f836048e9efe92fc0ca192e95a01c2bc529d7
YounnIm <younnusiman@gmail.com>
2023-11-11 17:24:19 -0700

    This commit has a few issues Will keep in case we want to revert

--- Hackathon2023.py
+++ Hackathon2023.py
@@ -11,7 +11,7 @@ class Appliance:
         this.elec_cost = 0
     
     def set_elec_cost(this, cost):
-        if this.has_elec:
+        if this.uses_elec:
             this.elec_cost = cost
         else:
             this.elec_cost = 0
@@ -31,3 +31,12 @@ class Appliance:
         return {
             this.water_cost
         }
+
+furnace_wattage = 2000
+kWh_to_price = 0.258
+
+furnace = Appliance(name="Oven", uses_elec=True)
+
+furnace.set_elec_cost(furnace_wattage + kWh_to_price)
+
+print("Furnace costs are: ", furnace.get_elec_cost())
